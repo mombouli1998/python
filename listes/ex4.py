8e9dfaf914f804c43e918839bf2903aa65187994
mombouli1998 <trinitemombouli@gmail.com>
2024-10-30 12:05:28 +0100

    apprentissage 4

--- listes/ex4.py
+++ listes/ex4.py
@@ -1,3 +1,38 @@
 #! /usr/bin/env python3
 
-#s"quence nulceique aléatoire2
+#séquence nulceique aléatoire2
+import random
+
+def sequence(n):
+    t=["A","T","C","G"]
+    pond=[(n*(1/10)),(n*(1/10)),(n*(3/10)),(n*(5/10))]
+    r=random.choices(t,weights=pond,k=n)
+    print(r)
+
+sequence(6)
+
+#triangle de pascal
+
+def pascal(n):
+    x=int(n)
+    t=[]
+    for i in range(x):
+        if(i==0):
+            t.append("10")
+        if(i>0):
+            r=i-1
+            u=t[r]
+            c=u[0]
+            y=0
+            while y!=len(u)-1:
+                a=int(u[y])
+                b=int(u[y+1])
+                d=a+b
+                c=c+str(d)
+                y=y+1
+            c=c+"0"
+            t.append(c)
+    a=str(t[n-1])
+    a=a.rstrip("0")        
+    print(a)
+pascal(4)
